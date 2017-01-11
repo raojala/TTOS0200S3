21f7e8c166b8a415299705ecefdcc99d0a940d4a
k8412 <k8412@ICT-421-WS-16>
2017-01-11 15:00:13 +0200

    TEHTÄVÄ 15

--- b_tehtava_15/Program.cs
+++ b_tehtava_15/Program.cs
@@ -11,56 +11,54 @@ namespace b_tehtava_15
         static void Main(string[] args)
         {
 
-            /* Tee ohjelma, joka tulostaa seuraavanlaisen kuvion. Kysy käyttäjältä puunkorkeus käytä juurena kaksi korkeuden yksikkö.
+            ///* Tee ohjelma, joka tulostaa seuraavanlaisen kuvion. Kysy käyttäjältä puunkorkeus käytä juurena kaksi korkeuden yksikkö.
 
-                Anna Luku > 7 [Enter]    
-                    *
-                   ***
-                  *****
-                 *******
-                *********
-                    *
-                    *
-            */
+            //    Anna Luku > 7 [Enter]    
+            //        *
+            //       ***
+            //      *****
+            //     *******
+            //    *********
+            //        *
+            //        *
+            //*/
 
-            // minimi korkeus 2
-            // ekan rivin pituus on korkeus-2 (jos korkeus syötetään  10, ekan rivin merkkejä 8 joista viimeinen on *
+            //// minimi korkeus 2
+            //// ekan rivin pituus on korkeus-2 (jos korkeus syötetään  10, ekan rivin merkkejä 8 joista viimeinen on *
 
-            int korkeus = 2; // tästä miinus 3 on ekan rivin pituus
-            string[] rivit;
+            //int korkeus = 2; // tästä miinus 3 on ekan rivin pituus
+            //string[] rivit;
 
-            Console.WriteLine("kuusen korkeus? (minimi 2)");
-            int.TryParse(Console.ReadLine(), out korkeus);
+            //Console.WriteLine("kuusen korkeus? (minimi 2)");
+            //int.TryParse(Console.ReadLine(), out korkeus);
 
-            if (korkeus < 2)
-            {
-                Environment.Exit(0);
-            }
+            //if (korkeus < 2)
+            //{
+            //    Environment.Exit(0);
+            //}
 
-            rivit = new string[korkeus];
+            //rivit = new string[korkeus];
 
-            // nollataan rivit ns.
-            for (int i = 0; i < korkeus; i++)
-            {
-                rivit[i] = "";
-            }
+            //// nollataan rivit ns.
+            //for (int i = 0; i < korkeus; i++)
+            //{
+            //    rivit[i] = "";
+            //}
 
-            for (int i = 0; i < korkeus; i++)
-            {
-                for (int x = korkeus - 3 + i; x > 0; x--)
-                {
-                    rivit[i] += '#';
-                }
-            }
+            //for (int i = 0; i < korkeus; i++)
+            //{
+            //    for (int x = korkeus - 3 + i; x > 0; x--)
+            //    {
+            //        rivit[i] += '#';
+            //    }
+            //}
 
-            for (int i = 0; i < korkeus; i++)
-            {
-                Console.WriteLine(rivit[i]);
-            }
+            //for (int i = 0; i < korkeus; i++)
+            //{
+            //    Console.WriteLine(rivit[i]);
+            //}
 
+            //KESKEN
         }
     }
 }
-// kikkelis kokkelis jee jee
-
-    // toinen muutos kommenttiin kekekekekekeke KIII
