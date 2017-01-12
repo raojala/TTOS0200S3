101d7f19fc6e4e772d22e34060e8d5dce37441d8
Rami <k8412@student.jamk.fi>
2017-01-12 08:06:03 +0200

    Kuusi valmis (harjoitus 15)

--- b_tehtava_15/Program.cs
+++ b_tehtava_15/Program.cs
@@ -11,54 +11,91 @@ namespace b_tehtava_15
         static void Main(string[] args)
         {
 
-            ///* Tee ohjelma, joka tulostaa seuraavanlaisen kuvion. Kysy käyttäjältä puunkorkeus käytä juurena kaksi korkeuden yksikkö.
+            int korkeus = 2; // tästä miinus 3 on ekan rivin pituus
+            string[] rivit;
+            string s = "";
 
-            //    Anna Luku > 7 [Enter]    
-            //        *
-            //       ***
-            //      *****
-            //     *******
-            //    *********
-            //        *
-            //        *
-            //*/
+            // tulostetaan kysymys, ja ajetaan if lauseen sulkujen sisällä integer korkeus muuttujaan
+            // (jos ei saada integeriä käyttäjän syötöstä, mennään else haaraan ja tulostetaan virhe
+            Console.WriteLine("kuusen korkeus? (minimi 2)");
+            if (int.TryParse(Console.ReadLine(), out korkeus))
+            {
+                // jos korkeus on liien pieni ni niin tulostetaan että liian pieni ja suljetaan ohjelma (koska ohjelmassa ei ole mainlooppia)
+                if (korkeus < 2)
+                {
+                    Console.WriteLine("Korkeus on liian pieni, ohjelma lopetetaan.");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
 
-            //// minimi korkeus 2
-            //// ekan rivin pituus on korkeus-2 (jos korkeus syötetään  10, ekan rivin merkkejä 8 joista viimeinen on *
+                rivit = new string[korkeus];
 
-            //int korkeus = 2; // tästä miinus 3 on ekan rivin pituus
-            //string[] rivit;
+                // tehdään sisennykset merkillä #
+                for (int i = 0; i < korkeus; i++)
+                {
+                    // eka rivi
+                    if (i == 0)
+                    {
+                        s = "";
+                        // mitä -3 tulee, katso kuva kuusirapellys.png
+                        for (int x = 0; x < korkeus-3; x++)
+                        {
+                            s += "#";
+                        }
+                        rivit[i] = s;
+                    }
+                    else // toisesta rivistä eteenpäin
+                    {
+                        s = "";
+                        for (int x = 0; x < korkeus - 3 - i; x++)
+                        {
+                            s += "#";
+                        }
+                        rivit[i] = s;
+                    }
+                }
 
-            //Console.WriteLine("kuusen korkeus? (minimi 2)");
-            //int.TryParse(Console.ReadLine(), out korkeus);
+                // tehdään merkit * kuusen muotoon sisennysten perään
+                for (int i = 0; i < korkeus; i++)
+                {
+                    // ensimmäinen rivi
+                    if (i == 0)
+                    {
+                        rivit[i] += "*";
+                    }
+                    else // toisesta rivistä eteenpäin
+                    {
+                        s = "";
+                        // guom i+(i+1):lla saadaan tarvittavien tähtien määrä tulosteeseen. taas katso kuva kuusirapellys.png
+                        // joka pitäisi olla solution explorerissa tehtava 15 alla.
+                        for (int x = 0; x < i+(i+1); x++)
+                        {
+                            s += "*";
+                        }
+                        rivit[i] += s;
+                    }
 
-            //if (korkeus < 2)
-            //{
-            //    Environment.Exit(0);
-            //}
+                    // muokataan kaksi viimeistä riviä vastaamaan ensimmäistä riviä jotta saadaan puun jalka
+                    rivit[rivit.Length -1] = rivit[0];
+                    rivit[rivit.Length -2] = rivit[0];
+                }
 
-            //rivit = new string[korkeus];
+                // vaihdetaan sisennyksen merkit '#' vastaamaan ' ' eli välilyöntiä.
+                for (int i = 0; i < korkeus; i++)
+                {
+                    rivit[i] = rivit[i].Replace('#', ' ');
+                }
 
-            //// nollataan rivit ns.
-            //for (int i = 0; i < korkeus; i++)
-            //{
-            //    rivit[i] = "";
-            //}
-
-            //for (int i = 0; i < korkeus; i++)
-            //{
-            //    for (int x = korkeus - 3 + i; x > 0; x--)
-            //    {
-            //        rivit[i] += '#';
-            //    }
-            //}
-
-            //for (int i = 0; i < korkeus; i++)
-            //{
-            //    Console.WriteLine(rivit[i]);
-            //}
-
-            //KESKEN
+                // tulostetaan puu
+                foreach (string ss in rivit)
+                {
+                    Console.WriteLine(ss);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not an integer!");
+            }
         }
     }
 }
