008e96d947e2fbc858bb462d293c16647542883c
Rami <k8412@student.jamk.fi>
2017-01-15 13:52:42 +0200

    dan and dan

--- b_tehtava_19/Program.cs
+++ b_tehtava_19/Program.cs
@@ -13,18 +13,70 @@ namespace b_tehtava_19
             /*
                 Tehtävä 19
                 Tee tekstipohjainen Hirsipuu-peli. 
-                Voit kovakoodata arvattavan sanan ja toteuta looppi, jossa käyttäjältä kysytään seuraavaa kirjainta.
+                Voit kovakoodata arvattavan sanan 
+                toteuta looppi, jossa käyttäjältä kysytään seuraavaa kirjainta.
                 Muista näyttää aina kirjaimen jälkeen oikein arvatut kirjaimet sanan oikealla kohdalla (käytä esim _-alaviivaa ei arvattujen kirjainten kohdalla).
                 Voit näyttää myös jo arvatut ei käytetyt -kirjaimet.
                 Päätä itse milloin pelaaja joutuu hirteen.
             */
 
-            int arvauksiaJaljella = 0;
-            string arvattavaSana = "Kakkapylly";
-            char arvaus;
+            int arvauksiaJaljella = 7;
+            string arvattavaSana = "kakkapylly", tuloste = "__________", arvatutKirjaimet = "";
+            char arvausArvo;
+            bool oikein;
+
+            StringBuilder sb = new StringBuilder(tuloste);
+
+            while (arvauksiaJaljella > 0)
+            {
+                Console.WriteLine(tuloste);
+
+                oikein = false;
+                arvausArvo = arvaus();
+
+                for (int i = 0; i < arvattavaSana.Length; i++)
+                {
+                    if (arvattavaSana[i] == arvausArvo)
+                    {
+                        oikein = true;
+                        sb[i] = arvausArvo;
+                        tuloste = sb.ToString();
+                    }
+                }
+
+                if (tuloste == arvattavaSana)
+                {
+                    Console.WriteLine("JEEEEE OIKEIN!");
+                    break;
+                }
 
 
+                if(!oikein)
+                {
+                    Console.WriteLine("VOE EI IHAN PÄIN PERSETTÄ MÄN!");
+                    arvauksiaJaljella--;
+                }
+            }
+        }
 
+        static char arvaus ()
+        {
+            char c;
+            Console.WriteLine("Arvaa Kirjain");
+            while (true)
+            {
+                // parsetaan tuloste c muuttujaan ja suljetaan looppi jos se on öökkönen
+                if (char.TryParse(Console.ReadLine(), out c) && char.IsLetter(c))
+                {
+                    char.ToLower(c);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Syötä vain YKSI KIRJAIN");
+                }
+            }
+            return c;
         }
     }
 }
--- b_tehtava_20/Program.cs
+++ b_tehtava_20/Program.cs
@@ -16,6 +16,12 @@ namespace b_tehtava_20
             // ajastin
             // huvikseni käytän goto kohta; korvaamaan looppeja testimielessä.
 
+            // periaatteessa toimii
+            // ohjelma jostain syystä menee rikki jossain välissä
+            // 600 ajastimella 500 kohdalla ajastin hyppää yhtäkkiä 140 kohtaan enkä tajua miksi.
+            // tekee saman aina tietyn ajan jälkeen muillakin numeroilla.
+            // koko looppi vaan juoksee itsensä kelausnappipohjassa loppuun.
+
             int timer = 0;
 
             // kysytään käyttäjältä montako sekuntia ajastimeen syötetään
@@ -30,19 +36,23 @@ namespace b_tehtava_20
             int now = DateTime.Now.Second;
 
         timerLoop:
-            if (now < DateTime.Now.Second)
             {
-                timer--;
-                now = DateTime.Now.Second;
-                if (now >= 59)
-                    now = 0;
-                Console.WriteLine(timer);
+                if (now < DateTime.Now.Second)
+                {
+                    timer--;
+                    now = DateTime.Now.Second;
+                    if (now >= 59)
+                        now = 0;
+                    Console.Clear();
+                    Console.WriteLine(timer);
+                    if (timer <= 0)
+                        goto stopProgram;
+                }
+                goto timerLoop;
             }
-            if (timer <= 0)
-                goto stopProgram;
-            goto timerLoop;
 
-        stopProgram:;
+        stopProgram: { }
+
         }
     }
 }
